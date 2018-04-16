ec1d9eb5691ce6732b2d638e91c6c5e1229651cf
Sarah Michaelson <sarah.michaelson@gmail.com>
2018-04-16 10:19:48 -0500

    Load README into home page

--- SampleWebApp/Pages/Index.cshtml.cs
+++ SampleWebApp/Pages/Index.cshtml.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Markdig;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.IO;
 
 namespace SampleWebApp.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Body;
+
         public void OnGet()
         {
+            Body = Markdown.ToHtml(Readme());
+        }
 
+        private string Readme()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("./README.md"))
+                {
+                    return sr.ReadToEnd();
+                }
+
+            }
+            catch
+            {
+                using (StreamReader sr = new StreamReader("./../README.md"))
+                {
+                    return sr.ReadToEnd();
+                }
+
+            }
         }
     }
 }
